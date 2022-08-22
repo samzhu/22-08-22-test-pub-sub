c84fd976cc78e0f7ceb8776e9d10174430ea39a3
samzhu <spike19820318@gmail.com>
2022-08-22 17:05:13 +0800

    fix: builder to 0.1.241-tiny

--- src/main/java/com/example/demo/ApplicationStartup.java
+++ src/main/java/com/example/demo/ApplicationStartup.java
@@ -1,15 +1,12 @@
 package com.example.demo;
 
-import java.util.Random;
 
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ApplicationStartup {
